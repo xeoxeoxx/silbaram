ffcdee62fd082681ff0d3a03f2e638cb515115ff
Seoyeong <a0123@nate.com>
2023-05-02 00:32:44 +0900

    a

--- src/main/java/com/project/silbaram/SilbaramApplication.java
+++ src/main/java/com/project/silbaram/SilbaramApplication.java
@@ -1,8 +1,10 @@
 package com.project.silbaram;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan
 @SpringBootApplication
 public class SilbaramApplication {
 
